8690397e04042d9008328459ee7ef8950ee89f51
Attila Vago <attilavago85@gmail.com>
2019-10-28 12:42:48 +0000

    initial commit, getting issues in json working

---
+++ index.js
@@ -0,0 +1,36 @@
+const Octokit = require("@octokit/rest");
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+dotenv.config();
+
+console.log(process.env.GITHUB_TOKEN);
+
+const octokit = Octokit({
+  auth: process.env.GITHUB_TOKEN,
+  userAgent: 'githubIssuesMastertool v.1.0.0',
+  baseUrl: 'https://api.github.com',
+  log: {
+    debug: () => {},
+    info: () => {},
+    warn: console.warn,
+    error: console.error
+  },
+  request: {
+    agent: undefined,
+    fetch: undefined,
+    timeout: 0
+  }
+});
+
+octokit.issues.listForRepo({
+  repo: 'Jaguar-Land-Rover',
+  owner: 'user-vision',
+  labels: 'Jaguar_ComparisonTool',
+}).then(({data, headers, status}) => {
+  fs.writeFile('issues.json', JSON.stringify(data), function (err) {
+    if (err) throw err;
+    console.log('Issues saved to issues.json!');
+  });
+  
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Tissue
+A Node.js based tool to get issues from Github and translate them to whatever else. 
