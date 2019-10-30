7e86bec30c39845a74da6da0984cde6d6cbe8186
Attila Vago <attilavago85@gmail.com>
2019-10-30 21:16:44 +0000

    filter issues by tag type, write headings

--- index.js
+++ index.js
@@ -37,21 +37,41 @@ const findLabel = (array) => {
   return labels;
 }
 
+const findIssueCategory = (issuesArray, categoryLabel) => {
+  let issues = [];
+  issuesArray.forEach(function(issue) {
+    issue.labels.forEach(function(label) {
+      if(label.name === categoryLabel) issues.push(issue);
+    });
+  });
+  return issues;
+}
+
+const writeHeading = (heading) => {
+  fs.appendFile('issues.md', `${heading}\n\r`, function (err) {
+    if (err) throw err;
+  });
+}
+
 octokit.issues.listForRepo({
   repo: repositoryName,//'Jaguar-Land-Rover'
   owner: projectOwner,//'user-vision'
   labels: issueLabel,//'Jaguar_ComparisonTool'
 }).then(({data, headers, status}) => {
+  // we need to sort the array of objects by: 1. Positive, 2. High Impact, 3. Medium Impact, 4. Low Impact, 5. Observation
+  
+  
   fs.writeFile('issues.json', JSON.stringify(data,null, 4), function (err) {
     if (err) throw err;
     console.log('Issues saved to issues.json!');
-    data.forEach(function (issue) {
+  
+    writeHeading('# Positive Findings');
+    
+    findIssueCategory(data, "P").forEach(function (issue) {
       let title = `${issue.body.split('\r')[0]}`;
       // # Keyboard on Desktop,Mobile,Tablet
       let titleWithLabels = `${issue.body.split('\r')[0]} ${findLabel(issue.labels).length !== 0 ? 'on' : ''} ${findLabel(issue.labels)}`;
       let body = issue.body.substring(title.length + 1);
-      //console.log(titleWithLabels);
-      //console.log(body);
       fs.appendFile('issues.md', `${titleWithLabels} ${body}`, function (err) {
         if (err) throw err;
         // console.log('Issue saved to issues.md!');
