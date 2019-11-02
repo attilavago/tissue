97ad42f3094a3e3fbb9bedc3f6da739f5ce3c39c
Attila Vago <attilavago85@gmail.com>
2019-11-02 20:09:03 +0000

    some more rules saved to json

--- generateWcag21.js
+++ generateWcag21.js
@@ -15,5 +15,26 @@ $('.panel-body-principle').each(function (i, elem) {
   json[i].description = $(this).text().trim();
 });
 
+json[0].guidelines = [];
+json[1].guidelines = [];
+json[2].guidelines = [];
+json[3].guidelines = [];
 
-console.log(json);
+$('.principle:nth-child(1) > .guidelines > .guideline > .panel-heading').each(function (i, elem) {
+  //console.log($(this.children[0].children[1]).text());
+  json[0].guidelines.push({id: $(this.children[0].children[1]).text(), title: $(this.children[0]).text().trim(), description: $(this.children[1]).text().trim()});
+});
+
+$('.principle:nth-child(2) > .guidelines > .guideline > .panel-heading').each(function (i, elem) {
+  json[1].guidelines.push({id: $(this.children[0].children[1]).text(), title: $(this.children[0]).text().trim(), description: $(this.children[1]).text().trim()});
+});
+
+$('.principle:nth-child(3) > .guidelines > .guideline > .panel-heading').each(function (i, elem) {
+  json[2].guidelines.push({id: $(this.children[0].children[1]).text(), title: $(this.children[0]).text().trim(), description: $(this.children[1]).text().trim()});
+});
+
+$('.principle:nth-child(4) > .guidelines > .guideline > .panel-heading').each(function (i, elem) {
+  json[3].guidelines.push({id: $(this.children[0].children[1]).text(), title: $(this.children[0]).text().trim(), description: $(this.children[1]).text().trim()});
+});
+
+console.log(JSON.stringify(json,null, 4));
